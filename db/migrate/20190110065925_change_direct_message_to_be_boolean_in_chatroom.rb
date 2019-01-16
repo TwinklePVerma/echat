4e5f2fcd7169152232aa21a57ee8dc4c0bc51f0d
Twinkle Verma <tverma@isystango.com>
2019-01-16 15:26:37 +0530

    made chatroom migration irreversible

--- db/migrate/20190110065925_change_direct_message_to_be_boolean_in_chatroom.rb
+++ db/migrate/20190110065925_change_direct_message_to_be_boolean_in_chatroom.rb
@@ -1,5 +1,9 @@
 class ChangeDirectMessageToBeBooleanInChatroom < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :chatrooms, :direct_message, :boolean, default: true
+  end
+
+  def down
+    change_column :chatrooms, :direct_message, :integer, default: 0
   end
 end
